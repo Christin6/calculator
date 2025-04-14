29f2d43fa4c3274f323d4d8ce1cb4344032036ca
Christin6 <94294443+Christin6@users.noreply.github.com>
2025-04-14 17:28:30 +0700

    make the calculator sorta work

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 let num1, num2, operator;
 let result = 0;
-let status = "num1";
+let numStatus = "num1";
 
 let display = document.getElementById("display");
 Array.from(document.getElementsByClassName("num")).forEach((x) => {
-    x.addEventListener("click", () => display.value += x.value);
+    x.addEventListener("click", () => addNumToDisplay(x.value));
 });
 
 let equalButton = document.getElementById("equal");
 let resetButton = document.getElementById("reset");
+
+Array.from(document.getElementsByClassName("operator")).forEach((y) => {
+    y.addEventListener("click", () => {
+        if(numStatus === "num1") {
+            num1 = parseInt(display.value);
+            resetDisplay();
+            numStatus = "num2";
+        } else if (numStatus === "num2") {
+            if (typeof num2 === "undefined"){
+                num2 = parseInt(display.value);
+            } else {
+                operate(num1, num2, operator);
+                display.value = result;
+                num1 = result;
+                num2 = "undefined";
+                numStatus = "num1";
+            }
+        }
+        operator = y.value;
+        console.log(num1);
+        console.log(num2);
+    });
+});
+
+resetButton.addEventListener("click", () => resetDisplay());
+
+function resetDisplay() {
+    display.value = "";
+}
+
+function addNumToDisplay(x) {
+    display.value += x;
+}
 
 function add (x, y) {
     return x + y;
