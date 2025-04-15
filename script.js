4876f54b64559341c0630923cba495e06fbd94fa
Christin6 <94294443+Christin6@users.noreply.github.com>
2025-04-15 19:04:39 +0700

    make error message of division with 0

--- script.js
+++ script.js
@@ -100,7 +100,11 @@ function operate(num1, num2, operator) {
             result = multiply(num1, num2);
             break;
         case "/":
-            result = divide(num1, num2);
+            if (num2 !== 0) {
+                result = divide(num1, num2);
+            } else {
+                result = "NAH, invalid";
+            }
             break;
         
     }
